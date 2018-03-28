a2cfcd1d7dfc5327e2596665f47f0d553cf969a9
fightzc <1393913851@qq.com>
2018-03-28 16:35:48 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,6 @@
 if('serviceWorker' in navigator) {
     //注册上一步创建的js文件
     navigator.serviceWorker
-        .register('./service-worker.js')
+        .register('/wintest1/service-worker.js')
         .then(function() { console.log('Service Worker Registered'); });
 }
