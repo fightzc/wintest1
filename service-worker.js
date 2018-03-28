a2e18aa0b01d6684ce3a1fac96dffa19de0a5ecb
fightzc <1393913851@qq.com>
2018-03-28 16:46:07 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,9 @@
  * Created by Administrator on 2018/3/28.
  */
 var cacheName = 'oslab-zc-blog';
-var filesToCache = [];
+var filesToCache = [
+    'test.html'
+];
 
 self.addEventListener('install', function(e){
     console.log('service worker install');
@@ -10,6 +12,20 @@ self.addEventListener('install', function(e){
         caches.open(cacheName).then(function(cache){
             console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(e){
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys.then(function(keyList){
+            return Promise.all(keyList.map(function(){
+                console.log('[ServiceWorker] Removing old cache', key);
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }))
         })
     );
 });
