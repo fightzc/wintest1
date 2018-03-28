98466d48626f312dba2ceef0ca886eca86b8ccf9
fightzc <1393913851@qq.com>
2018-03-28 18:11:16 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -20,7 +20,7 @@ self.addEventListener('install', function(e){
 self.addEventListener('activate', function(e){
     console.log('[ServiceWorker] Activate');
     e.waitUntil(
-        caches.keys.then(function(keyList){
+        caches.keys().then(function(keyList){
             return Promise.all(keyList.map(function(){
                 console.log('[ServiceWorker] Removing old cache', key);
                 if (key !== cacheName) {
