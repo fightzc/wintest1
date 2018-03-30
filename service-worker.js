53a76a969869c30efd0569a57e4820ca17a2e302
fightzc <1393913851@qq.com>
2018-03-30 15:55:59 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -10,12 +10,10 @@ var filesToCache = [
 self.addEventListener('install', function(e){
     console.log('service worker install');
     e.waitUntil(
-        caches.open(cacheName).then(function(cache){
-            console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
-        }).then(function(){
-            self.skipWaiting();
-            })
+        caches
+            .open(cacheName)
+            .then(function() { _self.skipWaiting();  })
+            .then(function() { console.log('[ServiceWorker] Install success'); })
     );
 });
 
