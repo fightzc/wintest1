7d2b133529bc804fdcbc87a67e9f916610dcad53
fightzc <1393913851@qq.com>
2018-04-03 15:10:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,5 +6,13 @@ if('serviceWorker' in navigator) {
     //注册上一步创建的js文件
     navigator.serviceWorker
         .register('/wintest1/service-worker.js')
-        .then(function() { console.log('Service Worker Registered'); });
+        .then(function() { console.log('Service Worker Registered'); })
+         .then(function()
+            {
+                const controller = navigator.serviceWorker.controller;
+                if (!controller) {
+                    return;
+                }
+                controller.postMessage('hellow', []);
+            })
 }
