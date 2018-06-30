c289733b155b6cc9b91441fb85fac02e586449ce
fightzc <1393913851@qq.com>
2018-06-30 16:02:46 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,79 +1,31 @@
-/**
- * Created by Administrator on 2018/3/28.
- */
-var cacheName = 'oslab-zc-blog-6';
-var filesToCache = [
-    'test.html',
-    '1.png'
-];
+const
+    version = '1.0.0',
+    CACHE = 'test' + version,
+    offlineURL = '/wintest1/',
+    installFilesEssential = [ '/', '1.png' ].concat(offlineURL);
 
-self.addEventListener('install', function(e){
-    console.log('service worker install');
-    //使用self.skipWaiting触发activate
-    e.waitUntil(
-        caches
-            .open(cacheName)
-            .then(function() { self.skipWaiting();  })
-            .then(function() { console.log('[ServiceWorker] Install success'); })
+self.addEventListener('install', event => {
+    console,log('server worker install');
+    event.waitUtill(
+        caches.open(CACHE)
+            .then(cache => {
+            cache.addAll(installFilesEssential);
+            })
+            .then(() => self.skipWaiting())
     );
 });
 
-self.addEventListener('activate', function(e){
-    console.log('[ServiceWorker] Activate');
-    e.waitUntil(
-        caches.keys().then(function(keyList){
-            return Promise.all(keyList.map(function(key){
-                console.log('[ServiceWorker] Removing old cache', key);
-                if (key !== cacheName) {
-                    return caches.delete(key);
-                }
-            }))
+self.addEventListener('activate', event => {
+    console.log('server worker activate');
+    event.waitUtill(
+        caches.keys()
+            .then(keylist => {
+            return Promise.all(
+                keylist
+                    .filter(key => $key !== CACHE)
+                    .map(key => caches.delete(key))
+            )
         })
+        .then(() => self.clients.claim())
     );
-});
-
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(respond){
-            return respond || fetch(e.request)
-                    .then(function(res){
-                        return caches.open(cacheName).then(function(cache){
-                            if (e.request.url.endsWith("jpg") ||
-                                e.request.url.endsWith("jpeg") ||
-                                e.request.url.endsWith("png") ||
-                                e.request.url.endsWith("bmp") ||
-                                e.request.url.endsWith("gif") ||
-                                e.request.url.endsWith("css") ||
-                                e.request.url.endsWith("js") ||
-                                e.request.url.endsWith("woff")){
-                                cache.put(e.request.url, res.clone());
-                            } else {
-                                console.log('=====url::', e.request.url);
-                            }
-                            return res;
-                        });
-                        // return res;
-                    })
-                    .catch(function(){
-                        return caches.match('test.html');
-                    });
-        })
-    )
-});
-
-// 取消添加到主屏幕
-self.addEventListener('beforeinstallprompt', function(e) {
-    e.preventDefault();
-    return false;
-});
-
-function sendMsg(msg) {
-    const controller = navigator.serviceWorker.controller;
-
-    if (!controller) {
-        return;
-    }
-
-    controller.postMessage(msg, []);
-}
-
+})
