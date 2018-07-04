a5f678ad143bb16211ee2dbc92dce7c30255acee
fightzc <1393913851@qq.com>
2018-07-04 22:53:26 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -32,7 +32,10 @@ self.addEventListener('activate', function(event){
             return Promise.all([
                 clients.claim(), //更新客户端
                 keyList.map(function(key){
-                    caches.delete(cache_name)
+                    if (key != cache_name){
+                         caches.delete(cache_name)
+                        }
+                  
                    
                 })
             ])
